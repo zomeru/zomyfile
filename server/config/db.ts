1446940c3396a73f0c1d84f278a57b2ba6ae5c9f
Zomer <zomerzxc.19@gmail.com>
2021-08-30 00:37:10 +0800

    Add DB and Schema

---
+++ server/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGODB_URI as string, {});
+  } catch (error: any) {
+    console.log('Connection Error', error.message);
+  }
+
+  const connection = mongoose.connection;
+  if (connection.readyState >= 1) {
+    console.log('Connected to database.');
+  }
+
+  connection.on('error', () => console.log('Connection failed.'));
+};
+
+export default connectDB;
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db';
 
 const app = express();
 dotenv.config();
+
+connectDB();
 
 app.use(cors());
 
