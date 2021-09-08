d744c5a0cb8d6b58377b7c3ed0da92ff31d40b1a
Zomer <zomerzxc.19@gmail.com>
2021-09-09 02:29:06 +0800

    Add download page

--- client/src/components/DownloadFile.tsx
+++ client/src/components/DownloadFile.tsx
@@ -4,17 +4,18 @@ import Image from 'next/image';
 const DownloadFile = ({ downloadPageLink }) => {
   return (
     <div className='p-1'>
-      <h1 className='my-2 text-lg font-medium'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-        quaerat?
-      </h1>
-      <div className='flex space-x-3 text-red-600 break-all '>
-        <span className='text-blue-500 break-all'>{downloadPageLink}</span>
+      <h1 className='my-2 text-lg font-medium'>Your download link is ready.</h1>
+      <div className='flex space-x-3 text-red-600 break-all'>
+        <span className='text-blue-500 break-all px-10 p-4'>
+          <a href={downloadPageLink} target='_blank' rel='noreferrer noopener'>
+            {downloadPageLink}
+          </a>
+        </span>
         <Image
           src='/images/copy.png'
           alt=''
-          width={30}
-          height={30}
+          width={40}
+          height={40}
           className='object-contain cursor-pointer'
           onClick={() => navigator.clipboard.writeText(downloadPageLink)}
         />
--- client/src/pages/download/[id]/index.tsx
+++ client/src/pages/download/[id]/index.tsx
@@ -2,11 +2,22 @@ import RenderFile from '@components/RenderFile';
 import axios from 'axios';
 import { IFile } from 'libs/types';
 import { GetServerSidePropsContext, NextPage } from 'next';
+import fileDownload from 'js-file-download';
 import Image from 'next/image';
 
 const index: NextPage<{ file: IFile }> = ({
   file: { format, name, sizeInBytes, id },
 }) => {
+  const handleDownload = async () => {
+    const { data } = await axios.get(
+      `http://localhost:8000/api/files/${id}/download`,
+      {
+        responseType: 'blob',
+      }
+    );
+    fileDownload(data, name);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center py-3 space-y-4 bg-gray-800 rounded-md shadow-xl w-96'>
       {!id ? (
@@ -21,7 +32,9 @@ const index: NextPage<{ file: IFile }> = ({
           />
           <h1 className='text-xl'>Your file is ready to be downloaded.</h1>
           <RenderFile file={{ format, name, sizeInBytes }} />
-          <button className='button'>Download</button>
+          <button className='button' onClick={handleDownload}>
+            Download
+          </button>
         </>
       )}
     </div>
