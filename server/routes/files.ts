39906a3ae2d7c092bdc5040b542d7f88f2d0eba2
Zomer <zomerzxc.19@gmail.com>
2021-09-09 03:36:38 +0800

    Implement Email sender

--- server/routes/files.ts
+++ server/routes/files.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import multer from 'multer';
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import https from 'https';
+import nodemailer from 'nodemailer';
 import File from '../models/File';
+import createEmailTemplate from '../utils/createEmailTemplate';
 
 const router = express.Router();
 
@@ -95,6 +97,75 @@ router.get('/:id/download', async (req, res) => {
   } catch (error) {
     return res.status(500).json({ message: 'Server Error :(' });
   }
+});
+
+router.post('/email', async (req, res) => {
+  //? 1. validate request
+  const { id, emailFrom, emailTo } = req.body;
+  if (!id || !emailTo || !emailFrom) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: 'Invalid data',
+    });
+  }
+
+  //? 2. check if the file exists
+  const file = await File.findById(id);
+
+  if (!file) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'File does not exist',
+    });
+  }
+
+  //? 3. create transporter
+  let transporter = nodemailer.createTransport({
+    // @ts-ignore
+    host: process.env.SENDINBLUE_SMTP_HOST!,
+    port: process.env.SENDINBLUE_SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SENDINBLUE_SMTP_USER,
+      pass: process.env.SENDINBLUE_SMTP_PASSWORD,
+    },
+  });
+
+  //? 4. prepare the email data
+  const { filename, sizeInBytes } = file;
+
+  const fileSize = `${(Number(sizeInBytes) / (1024 * 1024)).toFixed(2)} MB`;
+  const downloadPageLink = `${process.env.API_BASE_ENDPOINT_CLIENT}download/${id}`;
+
+  const mailOptions = {
+    from: emailFrom, // sender address
+    to: emailTo, // list of receivers
+    subject: 'File shared with you!', // Subject line
+    text: `${emailFrom} just sent you a file.`, // plain text body
+    html: createEmailTemplate(emailFrom, downloadPageLink, filename, fileSize), // html body
+  };
+
+  //? 5. send email using transporter
+  transporter.sendMail(mailOptions, async (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: 'Fail',
+        message: 'Server error',
+      });
+    }
+
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+
+    await file.save();
+    return res.status(200).json({
+      status: 'Success',
+      message: 'Email sent!',
+    });
+  });
+
+  //? 6. save the and send the response
 });
 
 export default router;
